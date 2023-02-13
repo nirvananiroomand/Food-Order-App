227675e9be742dc8febfd5cfe26e368afb8d3125
nirvananiroomand <nirvana.niroomand@gmail.com>
2023-02-13 11:07:48 +0330

    form for adding meals to cart

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,46 @@
+import { useRef, useState } from "react";
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
+
+const MealItemForm = (props) => {
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const amountInputRef = useRef();
+
+    const submitHandler = e => {
+        e.preventDefault();
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+
+        if (
+            enteredAmount.trim().length === 0 ||
+            enteredAmountNumber < 1 ||
+            enteredAmountNumber > 10
+        ) {
+            setAmountIsValid(false);
+            return;
+        }
+
+        props.onAddToCart(enteredAmountNumber);
+    };
+
+    return (
+        <form className={classes.form} onSubmit={submitHandler}>
+            <Input
+                ref={amountInputRef}
+                label="Amount"
+                input={{
+                    id: "amount",
+                    type: "number",
+                    min: "1",
+                    max: "10",
+                    step: "1",
+                    defaultValue: "1",
+                }}
+            />
+            <button>+ Add</button>
+            {!amountIsValid && <p>Please Enter a Valid Amount(1-10)</p>}
+        </form>
+    );
+};
+
+export default MealItemForm;
