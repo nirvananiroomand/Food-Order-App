7d214fba036b8714ed2e0cda7c15e65371480c4e
nirvananiroomand <nirvana.niroomand@gmail.com>
2023-02-27 11:42:18 +0330

    pushed meals in loaded meals array

--- src/components/Meals/AvailabeMeals.js
+++ src/components/Meals/AvailabeMeals.js
@@ -31,33 +31,45 @@ import { useEffect } from "react";
 // ];
 
 const AvailableMeals = () => {
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch(
+                "https://console.firebase.google.com/project/food-order-app-5e0a1/database/food-order-app-5e0a1-default-rtdb/data/~2F/meals.json"
+            );
+            const responseData = await response.json();
+        };
 
-  useEffect(() => {
-    const fetchMeals = async () => {
-      const response = await fetch("https://console.firebase.google.com/project/food-order-app-5e0a1/database/food-order-app-5e0a1-default-rtdb/data/~2F/meals.json");
-      const responseData = await response.json();
-    };
+        const loadedMeals = [];
 
-    fetchMeals();
-  }, []);
+        for (const key in responseData) {
+            loadedMeals.push({
+                id: key,
+                name: responseData[key].name,
+                description: responseData[key].description,
+                price: responseData[key].price,
+            });
+        }
 
-  const mealsList = DUMMY_MEALS.map((meals) => (
-    <MealItem
-      key={meals.id}
-      id={meals.id}
-      name={meals.name}
-      description={meals.description}
-      price={meals.price}
-    />
-  ));
+        fetchMeals();
+    }, []);
 
-  return (
-    <section className={classes.meals}>
-      <Card>
-        <ul>{mealsList}</ul>
-      </Card>
-    </section>
-  );
+    const mealsList = DUMMY_MEALS.map((meals) => (
+        <MealItem
+            key={meals.id}
+            id={meals.id}
+            name={meals.name}
+            description={meals.description}
+            price={meals.price}
+        />
+    ));
+
+    return (
+        <section className={classes.meals}>
+            <Card>
+                <ul>{mealsList}</ul>
+            </Card>
+        </section>
+    );
 };
 
 export default AvailableMeals;
